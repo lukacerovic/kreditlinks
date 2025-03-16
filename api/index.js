b00b85088f681fb6e95e20aed4a96364f6a76a68
lukacerovic <124380132+lukacerovic@users.noreply.github.com>
2025-03-16 17:31:49 +0100

    fixing cors links index.js

--- api/index.js
+++ api/index.js
@@ -16,7 +16,7 @@ const app = express();
 const allowedOrigins = [
   'http://localhost:3001',
   'http://localhost:3000',
-  'https://www.KreditLinks.rs',
+  'https://www.kreditlinks.com',
 ];
 
 app.use(cors({
