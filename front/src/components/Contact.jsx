d799b781a0729a0d02f77b076d8193c478185147
lukacerovic <124380132+lukacerovic@users.noreply.github.com>
2025-03-16 18:32:24 +0100

    Update Contact.jsx

--- front/src/components/Contact.jsx
+++ front/src/components/Contact.jsx
@@ -56,7 +56,7 @@ const Contact = forwardRef((props, ref) => {
       return;
     }
     try {
-        const response = await fetch('http://localhost:3001/api/users', {
+        const response = await fetch(`${process.env.REACT_APP_API_URL}/api/users`, {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
